4fb86e12e64842239ece3b433331e873df7f167a
E1jeong <won9964@gmail.com>
2025-03-14 17:16:44 +0900

    feat: 통계 화면에 BeeTablesCompose를 사용해서 dummy data 적용

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -84,6 +84,8 @@ dependencies {
 
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
+
+    implementation(libs.beetablescompose)
 }
 
 kapt {
--- presentation/src/main/java/com/queentech/presentation/main/statistic/StatisticScreen.kt
+++ presentation/src/main/java/com/queentech/presentation/main/statistic/StatisticScreen.kt
@@ -1,14 +1,38 @@
 package com.queentech.presentation.main.statistic
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.breens.beetablescompose.BeeTablesCompose
 import com.queentech.presentation.theme.FisherLottoTheme
 
 @Composable
 fun StatisticScreen() {
-    Text("StatisticScreen")
+    val winnerDatas = listOf(
+        WinnerData(1161, 1, 3, 50),
+        WinnerData(1160, 2, 10, 32),
+        WinnerData(1159, 1, 7, 28),
+        WinnerData(1158, 2, 6, 75),
+    )
+
+    val tableHeader = listOf("회차", "1등", "2등", "3등")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ) {
+        BeeTablesCompose(
+            data = winnerDatas,
+            headerTableTitles = tableHeader,
+            headerTitlesBackGroundColor = Color.LightGray
+        )
+    }
 }
 
 @Composable
@@ -20,3 +44,10 @@ fun StatisticScreenPreview() {
         }
     }
 }
+
+data class WinnerData(
+    val drawNumber: Int,
+    val firstPrize: Int,
+    val secondPrize: Int,
+    val thirdPrize: Int,
+)
